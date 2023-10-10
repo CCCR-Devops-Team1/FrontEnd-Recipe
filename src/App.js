242c3c16be7dd52cf8b26d4c64527812a4ed974f
cccrstd <voreja@naver.com>
2023-10-10 10:52:32 +0900

    front first project

--- src/App.js
+++ src/App.js
@@ -10,30 +10,13 @@ function App() {
   return (
     <div className="App">
 
-      {/* <div className='login-box'>
-        <h1>Food recommendation</h1>
-
-        <div className='login-main'>
-
-          <h3>enter your details to sign in to your account</h3>
-          <input type='text' className='user' placeholder='| ID'></input>
-          <input type='password' className='pass' placeholder='| PW'></input>
-        </div>
-
-        <div className='login-footer'>
-          <span>Don't have an account?</span>
-          <a href='#'>Signup Now</a>
-        </div>
-
-      </div> */}
-      
       <nav>
-        
-
-      <Link to="Signup" className="Sig">Signup Now</Link>
-      <p></p>
-      <Link to="Home" className="Sig">Home</Link>      
+        <Link to="Signup" className="Sig">Signup Now</Link>
+        <p></p>
+        <Link to="Home" className="Sig">Home</Link>      
       </nav> 
+
+
       <Routes>
         <Route path='Signup' element={<Signup/>}/>
         <Route path='Home' element={<Home/>}/>
