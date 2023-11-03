479f83cc69499066094fb898a3d172dc9e2fc337
cccrstd <voreja@naver.com>
2023-11-03 18:19:07 +0900

    11-03

--- src/component/url.js
+++ src/component/url.js
@@ -1,5 +1,5 @@
 export const MEMBERLOCAL = 'http://localhost:8081'
 export const NOTICELOCAL = 'http://localhost:8082'
-export const MEMBERPROD = 'http://recipe-member'
-export const NOTICEPROD = 'http://recipe-notice'
+export const MEMBERPROD = 'https://recipe-member'
+export const NOTICEPROD = 'https://recipe-notice'
     
