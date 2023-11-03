b38e23bb05f915895190f4fcfdd810619d8c9af6
cccrstd <voreja@naver.com>
2023-11-03 11:14:22 +0900

    11-03

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     const handleSubmit = async () => {
     
         try {
-            const response = await axios.post(`${MEMBERPROD}/user/login`,logindata);
+            const response = await axios.post(`${MEMBERPROD}/member/user/login`,logindata);
             if(response.data.code===200){
 
             setCookie("access_token",response.data.result.access_token,{
