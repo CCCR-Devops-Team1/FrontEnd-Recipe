425a28932ed0e930b79a56f2f9f2c720cd4858ff
hiljo <voreja@naver.com>
2023-11-04 21:55:03 +0900

    11-04

---
+++ src/component/setupProxy.js
@@ -0,0 +1,12 @@
+// const {createProxyMiddleware} = require('http-proxy-middleware')
+
+// module.exports = function(app) {
+//     app.use('/api',
+//         createProxyMiddleware(
+//             {
+//                 target: 'http://recipe-member',
+//                 changeOrigin: true,
+//             }
+//         )
+//     )
+// }
